89b031a774e208b139472e5cf8b81f0c8f530565
Antonín Kozák <kozak.tonda@hotmail.com>
2024-04-03 15:37:28 +0200

    Basic sender and receiver

---
+++ src/main/java/Send.java
@@ -0,0 +1,33 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.IntStream;
+
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            IntStream.range(0, 10)
+                            .parallel()
+                            .forEach(i -> {
+                                try {
+                                    String m = message + "_"+i;
+                                    channel.basicPublish("", QUEUE_NAME, null, m.getBytes(StandardCharsets.UTF_8));
+                                    System.out.println(" [x] Sent '" + m + "'");
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            });
+        }
+    }
+}
